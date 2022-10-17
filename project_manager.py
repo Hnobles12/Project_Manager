6347a36919c1ae2dbd58555af9bf07529fd2834d
hnobles12 <hnobles12@gmail.com>
2022-10-17 11:42:03 -0500

    Updated model details listbox size

--- project_manager.py
+++ project_manager.py
@@ -116,7 +116,7 @@ class ProjWin:
                 self.proj_data.get('PROJ_TVE'), autoscroll=True, key='_PROJ_TVE_', size=(40, 5))],
         ])
         top_row_model_details_frame2 = sg.Frame('Model Details:', layout=[
-            [sg.Column(layout=[[sg.Text('Models: ', size=(15, 1))],[sg.Button('Open', key='_OPEN_MODEL_'), sg.FilesBrowse('Add Files',enable_events=True, target='_ADD_MODEL_FILES_', key='_ADD_MODEL_FILES_', initial_folder=self.proj_path+'/Models')]]), sg.Listbox(self.models_files, key='_MODELS_LB_', size=(20, 4)), ],
+            [sg.Column(layout=[[sg.Text('Models: ', size=(15, 1))],[sg.Button('Open', key='_OPEN_MODEL_'), sg.FilesBrowse('Add Files',enable_events=True, target='_ADD_MODEL_FILES_', key='_ADD_MODEL_FILES_', initial_folder=self.proj_path+'/Models')]]), sg.Listbox(self.models_files, key='_MODELS_LB_', size=(30, 5)), ],
         ])
 
         top_row_stats_frame = sg.Frame('Details:', layout=[[sg.Text(f'Created: {self.proj_data.get("created")}')],[sg.Text(f'Updated: {self.proj_data.get("updated")}')]])
